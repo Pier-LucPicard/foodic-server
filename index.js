a9637ccd34e669b203e677330ec54a648fba3a5f
Pier-Luc Picard <pierluc_picard@yahoo.ca>
2016-09-15 15:50:40 -0400

    Adding tingobd

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var Engine = require('tingodb')();
 var config = require('./config/dev.json')
 
 app.get('/',function(req,res){
@@ -7,4 +8,5 @@ app.get('/',function(req,res){
   res.send("Hello World");
 });
 
+var db = new Engine.Db('./data',{});
 app.listen(config.server.port);
